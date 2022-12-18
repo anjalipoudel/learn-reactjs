e20e53aa2f0fad76f7d0c5b2df5c75ee3868d0c7
anjalipoudel <sabnom77poudel@gmail.com>
2022-12-18 17:19:47 +0545

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function formatName(user) {
+
+  return user.firstName + " " + user.lastName;
+}
+const user = {
+  firstName: 'Anjali',
+  lastName: 'Poudel'
+};
+
+const element =
+  ReactDOM.createRoot(document.getElementById('root'));
+element.render(
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
